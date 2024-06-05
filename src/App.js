7be1ec275784831ebb9dcc040e2623e0e41e58a4
Daksh-Malik <malikdaksh224@gmail.com>
2024-06-05 17:52:56 +0530

    "App Added"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react'
+
+let globalID = 0
 
 function App() {
+
+  const [task, setTask] = useState("");
+  const [list, setList] = useState({});
+
+  function addToList(){
+    const id = Date.now().toString();
+    setList(prev => {
+      setTask("");
+      if(task==="") return prev;
+      return {...prev, [id]: {todo: task, id, completed: false}};
+    });
+  }
+
+  function deleteItem(itemID){
+    setList(prev => {
+      const {[itemID]: _, ...rest} = prev; //destructuring and storing rest of the key-value pairs
+      return rest;
+    })
+  }
+
+  function checkEnter(e){
+    if(e.keyCode === 13){
+      addToList();
+    }
+  }
+
+  function done(itemID){
+    setList(prev => {
+      return {...prev, [itemID]: {...prev[itemID], completed: !prev[itemID].completed}}
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+        <h1>Basic To Do App</h1>
+        <div className='input-box'>
+          <input type='text' value={task} onChange={e => {setTask(e.target.value)}} onKeyDown={checkEnter}/>
+          <button id='add' onClick={addToList}></button>
+        </div>
+        <div id='outer-container'>
+          <div id='inner-container'>
+            <ul>
+              {Object.values(list).map((item) => {
+                return <div key={item.id} className={`item ${item.completed ? 'completed' : ''}`}>
+                  <div>
+                    <li className={item.completed && 'completed'}>{item.todo}</li>
+                  </div>
+                  <button id='done' onClick={()=>done(item.id)}></button>
+                  <button id='remove' onClick={()=>deleteItem(item.id)}></button>
+                </div>
+              })}
+            </ul>
+          </div>
+        </div>
     </div>
   );
 }
